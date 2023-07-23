f38ce608559a9dc950e309d08a47df90af26ac04
Brian Peng <bpengwin@Brians-Mac-mini.local>
2023-07-23 15:43:31 -0400

    mock data and endpoints created

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response
 import smtplib
-from email.mime.text import MIMEText
+from datetime import datetime
+import requests
 
 app = Flask(__name__)
 
@@ -9,15 +10,25 @@ app = Flask(__name__)
 database = {
     "users": [
         {
+            "id": 1,
             "name": "Dohyun Moon",
+            "phone": "1234567890",
             "email": "1ml84h2o@gmail.com",
             "drivers_license": "ABC123"
         }
     ],
+    "user_vehicle": [
+        {
+            "user_id": 1,
+            "vehicle_id": 1
+        }
+    ],
     "vehicles": [
         {
+            "id": 1,
             "license_plate": "XYZ987",
-            "user_id": 0  # Index of the user in the "users" list
+            "model": "Corolla",
+            "make": "Toyota"
         }
     ]
 }
@@ -25,14 +36,23 @@ database = {
 @app.route('/confirm_user_vehicle', methods=['GET'])
 def confirm_user_vehicle():
     data = request.json
+    name = data.get('name')
+    drivers_license = data.get('drivers_license')
     license_plate = data.get('license_plate')
+    
 
-    if not license_plate:
+    if not license_plate or not drivers_license or not name:
         return jsonify({"message": "Bad Request"}), 400
 
     vehicle = next((v for v in database['vehicles'] if v['license_plate'] == license_plate), None)
     if not vehicle:
         return jsonify({"message": "Vehicle not found"}), 404
+
+    pair = next((id for id in database['user_vehicle'] if id['vehicle_id'] == vehicle['id']))
+    user = next((u for u in database['users'] if u['id'] == pair['user_id']))
+    
+    if user["drivers_license"] != drivers_license or user["name"] != name:
+        return jsonify({"message": "User-Vehicle relationship does not match"}), 404
 
     return jsonify({"message": "User-Vehicle relationship confirmed"}), 200
 
@@ -40,41 +60,40 @@ def confirm_user_vehicle():
 @app.route('/create_user_ticket', methods=['POST'])
 def create_user_ticket():
     data = request.json
-    user_email = data.get('user_email')
     license_plate = data.get('license_plate')
+    type = data.get('type')
 
-    if not user_email or not license_plate:
+    if not license_plate or not type:
         return jsonify({"message": "Bad Request"}), 400
-
-    user = next((u for u in database['users'] if u['email'] == user_email), None)
-    if not user:
-        return jsonify({"message": "User not found"}), 404
 
     vehicle = next((v for v in database['vehicles'] if v['license_plate'] == license_plate), None)
     if not vehicle:
         return jsonify({"message": "Vehicle not found"}), 404
+    else:
+        pair = next((id for id in database['user_vehicle'] if id['vehicle_id'] == vehicle['id']))
+        user = next((u for u in database['users'] if u['id'] == pair['user_id']))
+        drivers_license = user['drivers_license']
+        cost = 100.00
 
-    # Create the ticket in your database (this is just a mock, so we do nothing here)
+        issue_date = datetime.now()
+        due_date = datetime.now() + datetime.timedelta(days=21)
 
-    # Send email notification to the user
-    send_email(user_email, "Ticket Created", "Your ticket has been created.")
-
-    return jsonify({"message": "Ticket created successfully"}), 200
-
-
-def send_email(to_email, subject, body):
-    # This function is used to send emails (mock implementation)
-    from_email = "noreply@example.com"
-    msg = MIMEText(body)
-    msg['Subject'] = subject
-    msg['From'] = from_email
-    msg['To'] = to_email
-
-    # Mock sending email (display message in console)
-    print("Sending email to:", to_email)
-    print("Subject:", subject)
-    print("Body:", body)
-    print("Email sent successfully")
+        json_data = {
+            "drivers_license": drivers_license,
+            "license_plate": license_plate,
+            "cost": cost,
+            "type": type,
+            "issue_date": issue_date,
+            "due_date": due_date
+        }
+        
+        res = make_response(jsonify(json_data), 200)
+        
+        API_ENDPOINT = ""
+        # API_KEY = ""
+        r = requests.post(url=API_ENDPOINT, data=json_data)
+        
+        return res
 
 
 if __name__ == '__main__':
