2e125b99c341059286c2f7c5323b1e6e7c9103d2
Brian Peng <bpengwin@Brians-Mac-mini.local>
2023-07-24 19:40:57 -0400

    fixed bugs with sending request to app server

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
 from flask import Flask, request, jsonify, make_response
 import smtplib
-from datetime import datetime
+from datetime import datetime, timedelta
 import requests
+import json
 
 app = Flask(__name__)
 
@@ -10,25 +11,64 @@ app = Flask(__name__)
 database = {
     "users": [
         {
-            "id": 1,
-            "name": "Dohyun Moon",
-            "phone": "1234567890",
-            "email": "1ml84h2o@gmail.com",
-            "drivers_license": "ABC123"
+            "id": 4,
+            "firebase_id": "1234gh5",
+            "name": "test1",
+            "phone_number": "905-333-3333",
+            "email": "test@uwaterloo.ca",
+            "drivers_licence": "dasjkh"
+        },
+        {
+            "id": 5,
+            "firebase_id": "qCarJOpIzVfjU1NpLkiV8Re0clW2",
+            "name": "Sarman Test 5",
+            "phone_number": "",
+            "email": "test5@sarman.com",
+            "drivers_licence": "D61044070950228"
+        },
+        {
+            "id": 6,
+            "firebase_id": "KXQ8a0w1i9Npchz3lu3Hx8YYA7u1",
+            "name": "Sarman Test 6",
+            "phone_number": "",
+            "email": "test6@sarman.com",
+            "drivers_licence": "23hdjskfh7824ej"
+        },
+        {
+            "id": 7,
+            "firebase_id": "EpTiMrZijtenURS6BmTpGP5LTq33",
+            "name": "Sarman Test 7",
+            "phone_number": "",
+            "email": "test7@sarman.com",
+            "drivers_licence": "halfajksfnjkhearted"
         }
     ],
     "user_vehicle": [
         {
-            "user_id": 1,
-            "vehicle_id": 1
+            "user_id": 5,
+            "vehicle_id": 6
+        },
+        {
+            "user_id": 5,
+            "vehicle_id": 7
         }
     ],
     "vehicles": [
         {
-            "id": 1,
-            "license_plate": "XYZ987",
-            "model": "Corolla",
-            "make": "Toyota"
+            "id": 6,
+            "licence_plate": "Asf",
+            "make": "Asf",
+            "model": "Asf",
+            "year": 0,
+            "province": "ontario"
+        },
+        {
+            "id": 7,
+            "licence_plate": "Sda",
+            "make": "Sda",
+            "model": "Sd",
+            "year": 2015,
+            "province": "ontario"
         }
     ]
 }
@@ -37,21 +77,22 @@ database = {
 def confirm_user_vehicle():
     data = request.json
     name = data.get('name')
-    drivers_license = data.get('drivers_license')
-    license_plate = data.get('license_plate')
+    drivers_licence = data.get('drivers_licence')
+    licence_plate = data.get('licence_plate')
+    province = data.get('province')
     
 
-    if not license_plate or not drivers_license or not name:
+    if not licence_plate or not drivers_licence or not name or not province:
         return jsonify({"message": "Bad Request"}), 400
 
-    vehicle = next((v for v in database['vehicles'] if v['license_plate'] == license_plate), None)
+    vehicle = next((v for v in database['vehicles'] if v['licence_plate'] == licence_plate and v['province'] == province), None)
     if not vehicle:
         return jsonify({"message": "Vehicle not found"}), 404
 
     pair = next((id for id in database['user_vehicle'] if id['vehicle_id'] == vehicle['id']))
     user = next((u for u in database['users'] if u['id'] == pair['user_id']))
     
-    if user["drivers_license"] != drivers_license or user["name"] != name:
+    if user["drivers_licence"] != drivers_licence or user["name"] != name:
         return jsonify({"message": "User-Vehicle relationship does not match"}), 404
 
     return jsonify({"message": "User-Vehicle relationship confirmed"}), 200
@@ -60,42 +101,39 @@ def confirm_user_vehicle():
 @app.route('/create_user_ticket', methods=['POST'])
 def create_user_ticket():
     data = request.json
-    license_plate = data.get('license_plate')
+    licence_plate = data.get('licence_plate')
+    province = data.get('province')
+    cost = data.get('cost')
     type = data.get('type')
 
-    if not license_plate or not type:
+    if not licence_plate or not type:
         return jsonify({"message": "Bad Request"}), 400
 
-    vehicle = next((v for v in database['vehicles'] if v['license_plate'] == license_plate), None)
+    vehicle = next((v for v in database['vehicles'] if v['licence_plate'] == licence_plate), None)
     if not vehicle:
         return jsonify({"message": "Vehicle not found"}), 404
     else:
-        pair = next((id for id in database['user_vehicle'] if id['vehicle_id'] == vehicle['id']))
-        user = next((u for u in database['users'] if u['id'] == pair['user_id']))
-        drivers_license = user['drivers_license']
-        cost = 100.00
-
         issue_date = datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
-        due_date = (datetime.now() + datetime.timedelta(days=21)).strftime("%Y-%m-%dT%H:%M:%SZ")
+        due_date = (datetime.now() + timedelta(days=21)).strftime("%Y-%m-%dT%H:%M:%SZ")
 
         json_data = {
             "ticket": {
-                "drivers_license": drivers_license,
-                "license_plate": license_plate,
-                "province": "Ontario",
+                "licence_plate": licence_plate,
+                "province": province,
                 "cost": cost,
-                "type": type,
+                "penalty_type": type,
                 "issue_date": issue_date,
                 "due_date": due_date
             }
         }
         
         res = make_response(jsonify(json_data), 200)
+        print("res:",json_data)
         
-        API_ENDPOINT = ""
+        API_ENDPOINT = "https://rails-ticket-server-d195e679f8ce.herokuapp.com/api/v1/tickets"
         # API_KEY = ""
-        r = requests.post(url=API_ENDPOINT, data=json_data)
-        
+        r = requests.post(url=API_ENDPOINT, headers={ "Content-Type" : "application/json" }, json=json_data)
+        print(r)
         return res
 
 
